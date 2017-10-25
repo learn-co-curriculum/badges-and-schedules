35ae238d5d70dfea3a1ac6e6c2964f5c2dbb832b
jprosario <joshuaowens011@gmail.com>
2017-10-25 13:46:43 -0400

    cleaner solution for #assign_rooms

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  attendees.map.with_index(1) do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
 end
 
