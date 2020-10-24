2498569c1ce7600ea2f95cb8996067f5f82b168c
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-24 04:09:51 -0500

    commit tree a042d5f557571428e61c4df479c31a533cfb1600

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+    new_names = []
+    name.each do |names|
+        new_names << "Hello, my name is #{names}."
+    end
+    return new_names
+end
+
+def assign_rooms(name) 
+    new_names = []
+    name.each_with_index do |names, index|
+        new_names << "Hello, #{names}! You'll be assigned to room #{index+1}!"
+    end
+    return new_names
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badges|
+        puts badges
+    end
+    assign_rooms(attendees).each do |badges|
+        puts badges
+    end
+end
